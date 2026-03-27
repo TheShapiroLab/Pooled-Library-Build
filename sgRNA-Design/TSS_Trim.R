491ff723f1a560e630bc2e7a921a4e19d1a504da
Nicholas C. Gervais <nickgervais@live.ca>
2026-03-27 19:49:59 -0400

    Update TSS_Trim.R

--- sgRNA-Design/TSS_Trim.R
+++ sgRNA-Design/TSS_Trim.R
@@ -3,23 +3,19 @@ library(dplyr)
 
 ##################################################
 #' Instructions: 
-#' 1. set the guide_design_region to the length you would like
-#' 2. run the code, and ensure input and output file paths and names are correct. 
-#' 3. run. 
-
+#' 1. Set the guide_design_region. This tells the script how far upstream of the TSS to set as the downstream boundary of the target region.
+#' For example, if the guide_design_region is set to 0, the region immediately upstream of the TSS will define the end of the target region.
+#' 2. Run the code, and ensure input and output file paths and names are correct. 
 #################################################
 #### Guide design region
-#### Set guide length
-#################################################
-
-guide_design_region <- 90
-
+guide_design_region <- 0
 ######################################################
-#### Read in the file and convert into a single cell
 
+#### Read in the input .txt file and convert into a single cell
+#### For an example of what the input file should look like, see the Example_Input.txt file in this repository
 dt <- tibble(
   main = paste(
-    readLines("C:/Path-to-features/Prepped_Features.txt"), collapse = "") )
+    readLines("C:/Path-to-features/Example_Input.txt"), collapse = "") )
   
 dt_2 <-  tidyr::separate_rows(dt,main, sep = ",")
 
@@ -44,9 +40,8 @@ dt_4$gene_name <- gsub("[^[:alnum:]]|?", "", dt_4$gene_name_b)
 dt_4$promoter_sequence <- gsub(" ", "", dt_4$promoter_sequence_sp)
 dt_5 <- dt_4[,.(gene_name, promoter_sequence)]
 
-##########
 ### Bring in the xlsx file
-#########
+### If you want to design sgRNAs upstream of the start codon exclusively (and disregard the TSS entirely), you can modify the .xlsx and change every 5' UTR to 0. 
 gene_lookup <- readxl::read_xlsx("C:/Path-to-table/TSS_Data.xlsx") %>% 
   filter(row_number() >1 ) %>% 
   rename(gene_name = `Sorted by Chromosome`,
